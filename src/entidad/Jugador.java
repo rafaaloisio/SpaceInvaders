1d6e80ced25d8bb72927128790afeb5ba5898620
Nicolas114 <gonzaleznicolas574@gmail.com>
2020-12-18 22:51:29 -0300

    Ahora el premio de supertiro es temporal

--- src/entidad/Jugador.java
+++ src/entidad/Jugador.java
@@ -10,9 +10,9 @@ import visitor.*;
 
 public class Jugador extends Personaje{
 
-	private final int MAXIMA_VIDA = 1000;
+	private final int MAXIMA_VIDA = 100000;
 	private final int MAXIMO_GOLPE = 40;
-	
+
 	public Jugador(Tablero miTablero, Celda celda) {
 		super(miTablero, celda);
 		this.vida = MAXIMA_VIDA;
@@ -21,30 +21,29 @@ public class Jugador extends Personaje{
 		entidadgrafica = new EntidadGraficaJugador("/recursos/jugador/nave.png");
 		entidadgrafica.getImagen().setBounds(x, y, PIXEL, PIXEL);
 	}
-	
+
 	@Override
 	public void morir() 
 	{
-		
-		
+
+
 		//detengo el hilo al perder
 		miCelda.eliminarEntidad(this);
-		
+
 		miTablero.getLogica().setPerdi(true);
-		
+
 		this.vida = 0;
 		super.morir();
-		
+
 		System.err.println("EL JUGADOR PERDIÓ.");
-		
-		
-		
+
+
+
 	}
 
 	@Override
 	public void ejecutar() 
 	{
-
 	} 
 
 	public void mover() 
@@ -63,17 +62,17 @@ public class Jugador extends Personaje{
 		case KeyEvent.VK_D : //Derecha
 			this.moverDer();
 			break;
-	
-		
-		//sirve para mover a la nave para arriba y abajo, no se pide en el enunciado.
-	/*		
+
+
+			//sirve para mover a la nave para arriba y abajo, no se pide en el enunciado.
+			/*		
 		case KeyEvent.VK_W : //Arriba
 			this.moverArr();
 			break;
 		case KeyEvent.VK_S : //Abajo
 			this.moverAb();
 			break;
-	*/		
+			 */		
 		case KeyEvent.VK_SPACE :
 			this.disparar(this);
 			break;
@@ -84,34 +83,34 @@ public class Jugador extends Personaje{
 	public void moverIzq() 
 	{
 		if (miTablero.getCelda(x-1, y) != null) {
-			
+
 			if (miTablero.getCelda(x-1, y).cantEntidades() == 0) {
-				
+
 				miTablero.getCelda(x, y).eliminarEntidad(this);
 				x = x - 1;
 				miTablero.getCelda(x, y).agregarEntidad(this);
 				miCelda = miTablero.getCelda(x, y);
 				entidadgrafica.actualizar(miCelda);		
-				
+
 			}else { //else aceptar visitor, porque tuvo una colosion
 
 				Entidad[] entidadesCelda = miTablero.getCelda(x-1, y).getArregloEntidades();
-				
+
 				for (int i=0; i < entidadesCelda.length; i++) {
-					
+
 					if (entidadesCelda[i] != null) {
 						System.err.println(entidadesCelda[i].toString());
 						entidadesCelda[i].aceptar(miVisitor);
 					}
 				}
-				
+
 				//si no se hace esto,la nave queda en el lugar, pero impacta en el lugar al que se movió.
 				miTablero.getCelda(x, y).eliminarEntidad(this);
 				x = x - 1;
 				miTablero.getCelda(x, y).agregarEntidad(this);
 				miCelda = miTablero.getCelda(x, y);
 				entidadgrafica.actualizar(miCelda);		
-				
+
 			} 
 		}
 	}
@@ -119,15 +118,15 @@ public class Jugador extends Personaje{
 	public void moverDer() 
 	{
 		if (miTablero.getCelda(x+1, y) != null) {
-			
+
 			if (miTablero.getCelda(x+1, y).cantEntidades() == 0) {
-				
+
 				miTablero.getCelda(x, y).eliminarEntidad(this);
 				x = x + 1;
 				miTablero.getCelda(x, y).agregarEntidad(this);
 				miCelda = miTablero.getCelda(x, y);
 				entidadgrafica.actualizar(miCelda);		
-				
+
 			}else { //else aceptar visitor, porque tuvo una colosion
 
 				Entidad[] entidadesCelda = miTablero.getCelda(x+1, y).getArregloEntidades();
@@ -138,7 +137,7 @@ public class Jugador extends Personaje{
 						entidadesCelda[i].aceptar(miVisitor);
 					}
 				}
-				
+
 				//si no se hace esto,la nave queda en el lugar, pero impacta en el lugar al que se movió.
 
 				miTablero.getCelda(x, y).eliminarEntidad(this);
@@ -146,14 +145,14 @@ public class Jugador extends Personaje{
 				miTablero.getCelda(x, y).agregarEntidad(this);
 				miCelda = miTablero.getCelda(x, y);
 				entidadgrafica.actualizar(miCelda);		
-				
-				
-				
+
+
+
 			} 
 		}
 
 	}
-	
+
 	//no se pide en el enunciado
 	public void moverArr() 
 	{
@@ -201,28 +200,37 @@ public class Jugador extends Personaje{
 			} 
 		}
 	}
-	
+
 	public Disparo crearDisparo() {
 
 		/*
-		
+
 		Disparo toReturn = new DisparoJugador(miTablero,miTablero.getCelda(miCelda.getX(), miCelda.getY()),this.getGolpe());
 		if (this.golpe > this.MAXIMO_GOLPE) {
 			this.golpe = this.MAXIMO_GOLPE;
 		}
-	
+
 		return toReturn;
+
+		 */
+
+		DisparoJugador retorno = new DisparoJugador(miTablero,miCelda,this.getGolpe());
 		
-		*/
-		
-		return new DisparoJugador(miTablero,miCelda,this.getGolpe());
-		
+		if (this.golpe > this.MAXIMO_GOLPE) {
+			this.golpe -= 5;
+		}
+
+		else {
+			this.golpe = this.MAXIMO_GOLPE;
+		}
+
+		return retorno;		
 	}
 
 	public void aceptar(Visitor visitor) {
 		visitor.visitJugador(this);
 	}
-	
+
 
 
 }
